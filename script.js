e0ec89bc75b6f4336eab6d4a80e398eab0cb8632
Matheus <matheus.augustoreal@gmail.com>
2022-02-28 13:05:09 -0300

    Learn about animations

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-$(".titulo").click(() => {
-    console.log("Click")
-})
+// $(".titulo").click(() => {
+//     console.log("Click")
+// })
 
-$(".titulo").mouseenter(() => {
-    console.log("Mouse enter")
-})
+// $(".titulo").mouseenter(() => {
+//     console.log("Mouse enter")
+// })
 
-$(".titulo").mouseleave(() => {
-    console.log("Mouse leave")
-    $("#sair").show()
-})
+// $(".titulo").mouseleave(() => {
+//     console.log("Mouse leave")
+//     $("#sair").show()
+// })
 
-$(".titulo").dblclick(() => {
-    console.log("Double click")
-})
+// $(".titulo").dblclick(() => {
+//     console.log("Double click")
+// })
+
+// -------------------------- Animations
+// $("#button").click(() => {
+//     $("#square").fadeToggle()
+// })
+
+// $("#button").click(() => {
+//     $("#square").slideToggle()
+// })
+
+// $("#button").click(() => {
+//     $("#square").animate({width: 1000}, 3000)
+// })
